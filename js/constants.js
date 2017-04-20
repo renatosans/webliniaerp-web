3691a5fe607a2f5438b1ec0b0a334fec7ea5b3d0
Rafael Bispo <rafaoliveirabispo@gmail.com>
2017-04-20 00:28:20 -0300

    Correcao banco

--- js/constants.js
+++ js/constants.js
@@ -4,16 +4,16 @@ function baseUrl(){
 	var pasta = "";
 
 	if(location.hostname == 'localhost' || window.location.hostname.indexOf("192.168.") != -1)
-		pasta = "/wbl-web";
+		pasta = "/webliniaerp-web";
 
 	return location.protocol+'//'+location.hostname+pasta+'/';
 }
 
 function baseUrlApi(){
     if(location.hostname.indexOf("192.168.") != -1)
-        return "http://"+ location.hostname +"/wbl-api/";
+        return "http://"+ location.hostname +"/webliniaerp-api/";
 	else if(location.hostname == 'localhost')
-		return "http://localhost/wbl-api/";
+		return "http://localhost/webliniaerp-api/";
 	else
 		return location.protocol+'//'+location.hostname+location.pathname+'api/';
 }
