8bd5ef12a9c4d779d4547e352dbca09edcbfb573
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2017-10-16 10:57:45 -0200

    subindo apenas para poder baixar no outro computador

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+var AMBIENTE = 'CLIENTES';
+
+function baseUrl(){
+	var pos   = window.location.pathname.lastIndexOf("/");
+	var pasta = "";
+
+	if(location.hostname == 'localhost' || window.location.hostname.indexOf("192.168.") != -1 || window.location.hostname.indexOf("120.1.") != -1)
+		pasta = "/webliniaerp-web";
+
+	return location.protocol+'//'+location.hostname+pasta+'/';
+}
+
+function baseUrlApi(){
+	return 'http://192.168.1.35/webliniaerp-api/';
+}
---
+++ util/constants.php
@@ -0,0 +1,31 @@
+<?php
+	# Session lifetime of 3 hours
+	ini_set('session.gc_maxlifetime', 10800);
+
+	# Enable session garbage collection with a 1% chance of
+	# running on each session_start()
+	ini_set('session.gc_probability', 1);
+	ini_set('session.gc_divisor', 100);
+
+	define('AMBIENTE','CLIENTES');
+	define('PAGE', substr($_SERVER['SCRIPT_NAME'],strripos($_SERVER['SCRIPT_NAME'],'/')+1));
+	
+	if($_SERVER['SERVER_NAME'] == 'localhost' || strpos($_SERVER['SERVER_NAME'], "192.168.") === 0 || strpos($_SERVER['SERVER_NAME'], "120.1.") === 0){
+		define('URL_API','http://'. $_SERVER['SERVER_NAME'] .'/webliniaerp-api/');
+		define('URL_BASE','http://'. $_SERVER['SERVER_NAME'] .'/webliniaerp-web/');
+	}else{
+		define('URL_API','http://'.$_SERVER['SERVER_NAME'].'/api/');
+		define('URL_BASE','http://'.$_SERVER['SERVER_NAME'].'/');	
+	}
+
+	if(isset($_GET['nickname']))
+	    define('NICKNAME',$_GET['nickname']);	
+	else
+		define('NICKNAME','');
+
+	if(isset($_GET['template']) && $_GET['template'] != "")
+	    define('TEMPLATE',$_GET['template']);	
+	else
+		define('TEMPLATE',"vitrine");
+
+?>
