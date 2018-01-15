dfd6e7616822c4d6860594d8037a0cb51e3f6508
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2018-01-15 11:09:40 -0200

    constants merge

--- util/constants.php
+++ util/constants.php
@@ -14,7 +14,7 @@
 	
 	if($_SERVER['SERVER_NAME'] == 'localhost' || strpos($_SERVER['SERVER_NAME'], "192.168.") === 0 || strpos($_SERVER['SERVER_NAME'], "120.1.") === 0){
 		define('URL_API','http://'. $_SERVER['SERVER_NAME'] .'/~filipecoelho/webliniaerp-api/');
-		define('URL_BASE','http://'. $_SERVER['SERVER_NAME'] .'/');
+		define('URL_BASE','http://'. $_SERVER['SERVER_NAME'] .'/~filipecoelho/');
 	}else{
 		define('URL_API','http://'.$_SERVER['SERVER_NAME'].'/api/');
 		define('URL_BASE','http://'.$_SERVER['SERVER_NAME'].'/');	
