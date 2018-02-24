7c7b474f5f76a35e97ae54bd6c47675096d7aca9
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2018-02-24 16:30:30 -0300

    minhas conexoes locais

--- js/constants.js
+++ js/constants.js
@@ -5,11 +5,11 @@ function baseUrl(){
 	var pasta = "";
 
 	if(location.hostname == 'localhost' || window.location.hostname.indexOf("192.168.") != -1 || window.location.hostname.indexOf("120.1.") != -1)
-		pasta = "/webliniaerp-web";
+		pasta = "/~filipecoelho/webliniaerp-web-pagare";
 
 	return location.protocol+'//'+location.hostname+pasta+'/';
 }
 
 function baseUrlApi(){
-	return location.protocol +'//'+ location.hostname +'/webliniaerp-api/';
+	return location.protocol +'//'+ location.hostname +'/~filipecoelho/webliniaerp-api-pagare/';
 }
