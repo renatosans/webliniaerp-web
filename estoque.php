1c96dba7c29c435b603836a627bc3394f72f1541
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2018-03-02 16:11:32 -0300

    ajusets

--- estoque.php
+++ estoque.php
@@ -464,8 +464,7 @@
 												<td class="text-right">R$ {{ dup.vlr_duplicata | numberFormat : 2 : ',' : '.' }}</td>
 												<td class="text-center">
 													<button type="button" class="btn btn-xs btn-danger"
-														tooltip="Excluir duplicata" data-toggle="tooltip"
-														ng-if="(!dup.xml)">
+														tooltip="Excluir duplicata" data-toggle="tooltip">
 														<i class="fa fa-trash-o"></i>
 													</button>
 												</td>
