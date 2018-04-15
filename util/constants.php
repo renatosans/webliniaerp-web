3e450135dcd21f0678ceeda784a583dfeec24532
jheizer wandel <jheizer@hotmail.com>
2018-04-15 06:33:28 -0300

    alterações

--- util/constants.php
+++ util/constants.php
@@ -1,33 +1,3 @@
-<?php
-	# Enable session garbage collection with a 1% chance of
-	# running on each session_start()
-	ini_set('session.gc_probability', 1);
-	ini_set('session.gc_divisor', 100);
-
-	ini_set('max_execution_time', 1800);
-    ini_set('session.cookie_lifetime', 28800);
-    ini_set('session.cache_expire', 28800);
-    ini_set('session.gc_maxlifetime', 28800);
-
-	define('AMBIENTE','CLIENTES');
-	define('PAGE', substr($_SERVER['SCRIPT_NAME'],strripos($_SERVER['SCRIPT_NAME'],'/')+1));
-	
-	if($_SERVER['SERVER_NAME'] == 'localhost' || strpos($_SERVER['SERVER_NAME'], "192.168.") === 0 || strpos($_SERVER['SERVER_NAME'], "120.1.") === 0){
-		define('URL_API','http://'. $_SERVER['SERVER_NAME'] .'/~filipecoelho/webliniaerp-api/');
-		define('URL_BASE','http://'. $_SERVER['SERVER_NAME'] .'/~filipecoelho/webliniaerp-web/');
-	}else{
-		define('URL_API','http://'.$_SERVER['SERVER_NAME'].'/api/');
-		define('URL_BASE','http://'.$_SERVER['SERVER_NAME'].'/');	
-	}
-
-	if(isset($_GET['nickname']))
-	    define('NICKNAME',$_GET['nickname']);	
-	else
-		define('NICKNAME','');
-
-	if(isset($_GET['template']) && $_GET['template'] != "")
-	    define('TEMPLATE',$_GET['template']);	
-	else
-		define('TEMPLATE',"vitrine");
-
-?>
+version https://git-lfs.github.com/spec/v1
+oid sha256:31cdbf3665123f6d6f64e1a57f0b6f6ccc5ef44dd33387edf899c778a6c38574
+size 1192
