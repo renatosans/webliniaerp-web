ab300d9d7a990630d411456733383e2f567131b4
Rafael <rafaoliveirabispo@gmail.com>
2018-06-27 09:55:10 -0300

    correção chamado #795

--- js/angular-controller/relatorio-total-produto-estoque-controller.js
+++ js/angular-controller/relatorio-total-produto-estoque-controller.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c703d25243b0b37b82326e510e4d24cd91901e98f8467bf623dd33b9d6f1456
-size 8550
+oid sha256:8328d72624a29dad86e3fab2f178ac7347b7528a8ab3d475fbce393a84810579
+size 8571
