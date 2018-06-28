136d6c65dca9d0c8113ce1f5e3e6dabfcfac8661
Rafael <rafaoliveirabispo@gmail.com>
2018-06-28 15:30:48 -0300

    melhorias chamado #1000

--- js/angular-controller/pdv-controller.js
+++ js/angular-controller/pdv-controller.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0947b33ba495c20656eeda96ac87333239ddafdef99ff9525ccbdafac427b002
-size 189125
+oid sha256:862e16946e890cef235d7c64830aa1e31c9b7177fe173d23911c84c8eee6407b
+size 189153
