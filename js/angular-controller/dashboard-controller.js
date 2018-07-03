12ae6f73490364a28fc0d31d8b26ed9bcc334857
Filipe Mendonça Coelho <filipemcoelho@FILIPE-MACBOOK.local>
2018-07-03 10:17:27 -0300

    reverse

--- js/angular-controller/dashboard-controller.js
+++ js/angular-controller/dashboard-controller.js
@@ -809,7 +809,7 @@ app.controller('DashboardController', function($scope, $http, $window, UserServi
 						},
 
 						title: {
-							text: 'Vendas Hora Hora'
+							text: null
 						},
 
 						xAxis: {
@@ -820,13 +820,14 @@ app.controller('DashboardController', function($scope, $http, $window, UserServi
 							categories: [ 	'0', '00:00 - 01:00','01:00 - 02:00','02:00 - 03:00','03:00 - 04:00','04:00 - 05:00','05:00 - 06:00','06:00 - 07:00','07:00 - 08:00','08:00 - 09:00','09:00 - 10:00','10:00 - 11:00','11:00 - 12:00',
 											'12:00 - 13:00','13:00 - 14:00','14:00 - 15:00','15:00 - 16:00','16:00 - 17:00','17:00 - 18:00','18:00 - 19:00','19:00 - 20:00','20:00 - 21:00','21:00 - 22:00','22:00 - 23:00', '23:00 - 24:00'
 										],
-							title: null
+							title: null,
+							reversed: true
 						},
 
 						colorAxis: {
 							min: 0,
 							minColor: '#FFFFFF',
-							maxColor: Highcharts.getOptions().colors[0]
+							maxColor: '#31708f'
 						},
 
 						legend: {
